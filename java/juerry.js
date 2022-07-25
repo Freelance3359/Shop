07b55404ec3de268a9764c9d121541f7c4090d83
Freelance3359 <101235801+Freelance3359@users.noreply.github.com>
2022-07-25 20:25:42 +0300

    Delete java directory

--- java/juerry.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function ($) {
-    $("#fullpage").fullpage({
-        scrollbar: false,
-        navigation: true,
-        navigationTooltips: ['1', '2', '3', '4', '5',],
-        loopTop: true,
-        loopBottom: true,
-    });
-})
--- java/main.js
+++
@@ -1,6 +0,0 @@
-const burger = document.querySelector('.burger')
-const container = document.querySelector('.container')
-
-burger.addEventListener('click', () => {
-    container.classList.toggle('active')
-})
